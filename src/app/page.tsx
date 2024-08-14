223fe46bdd8149ebe4bddfaa1f077105dc3337d6
EntrenaDev <manuel.entrena@gmail.com>
2024-08-14 08:29:31 +0200

    feat: :triangular_flag_on_post: Added our first feature flag with Flagsmith

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import flagsmith from "@/utils/flagsmith";
 import Link from "next/link";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
+
+        {flags.isFeatureEnabled('search') &&
+          <div>
+            <label htmlFor="input_flag">Input Flat</label>
+            <input id="input_flag" name="input_flag" placeholder="example feature flag" />
+          </div>
+        }
+
       </div>
       <Link href="/about">About</Link>
     </main>
